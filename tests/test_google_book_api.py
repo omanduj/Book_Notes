9d3535a03bb0a40597a95ce221d1c79560459922
omanduj <omanduj@gmail.com>
2022-01-04 14:14:46 -0600

    Fixes Pytest

--- tests/test_google_book_api.py
+++ tests/test_google_book_api.py
@@ -1,11 +1,13 @@
 from google_book_api import __version__
-from google_book_api.book_finder import create_query, pick_book
+from google_book_api.book_finder import pick_book, get_books
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_result_amount():
-    assert len(create_query('food')) == 5
+    assert len(get_books('food')) == 5
+    assert len(get_books(1)) == 5
 
-def test_clean_data():
-    assert pick_book('FoOd') == {'food': <__main__.Book object at 0x10a359d80>, 'in defence of food': <__main__.Book object at 0x10a358fa0>, 'the comic book guide to growing food': <__main__.Book object at 0x10a359090>, 'technically food': <__main__.Book object at 0x10a359300>, 'fast food genocide': <__main__.Book object at 0x10a3592d0>}
+def test_get_books():
+    assert get_books('saddasdwsaddasdw') == None
+    assert get_books('') == None
